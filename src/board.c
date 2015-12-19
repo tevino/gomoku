af0fd0887ec83c96f97284ad3ea77ab791af648f
Tevin Zhang <mail2tevin@gmail.com>
2015-12-19 23:58:55 +0800

    Pass board size to AI

--- src/board.c
+++ src/board.c
@@ -35,7 +35,7 @@ int read_move(FILE *f, int *x, int *y){
   if (sscanf(buf, "%c%d\n", &xc, y) != 2){
     return -1;
   };
-  
+
   *x = letter_no(xc);
 
   return 0;
@@ -52,8 +52,10 @@ int new_move(Map *map, int x, int y, Player *winner){
     next = next==BLACK?WHITE:BLACK;
     return 0;
   }
+
   return -1;
 }
+
 
 void print_board(Map *map){
   puts("");
@@ -61,29 +63,29 @@ void print_board(Map *map){
     for (int x = -1; x < width; x++) {
       // legend of X (letters)
       if (y == -1) {
-	// padding space
-	if (x == -1) {
-	  printf("    ");
-	  continue;
-	}
+        // padding space
+        if (x == -1) {
+          printf("    ");
+          continue;
+        }
 
-	printf("%c ", 'A' + x);
-	continue;
+        printf("%c ", 'A' + x);
+        continue;
       }
 
       // legend of Y (numbers)
       if (x == -1) {
-	printf("%*d ", 3, y);
-	continue;
+        printf("%*d ", 3, y);
+        continue;
       }
 
       switch (map_at(map, x, y)){
-      case BLACK:
-	printf("%s", BLACK_STR); break;
-      case WHITE:
-	printf("%s", WHITE_STR); break;
-      default:
-	printf("%s", EMPTY_STR); break;
+        case BLACK:
+          printf("%s", BLACK_STR); break;
+        case WHITE:
+          printf("%s", WHITE_STR); break;
+        default:
+          printf("%s", EMPTY_STR); break;
       }
     }
     puts("");
@@ -102,7 +104,7 @@ int main (int argc, char *argv[]){
 
   char *b_cmd = argv[1];
   char *w_cmd = argv[2];
-    
+
   if (argc == 5) {
     width = atoi(argv[3]);
     height = atoi(argv[4]);
@@ -110,37 +112,41 @@ int main (int argc, char *argv[]){
       fprintf(stderr, "Invalid width: %s", argv[3]);
       return 1;
     }
-    
+
     if (height < MIN_WIDTH) {
       fprintf(stderr, "Invalid height: %s", argv[4]);
       return 1;
     }
   }
 
+  char height_s[10], width_s[10];
+  sprintf(height_s, "%d", height);
+  sprintf(width_s, "%d", width);
+
   Map *map = map_create(width, height);
 
   int b_fd, w_fd;
   pid_t b_pid, w_pid;
   FILE *b_f, *w_f;
-  
+
   printf("launching black AI: %s\n", b_cmd);
-  
-  b_pid = popen2(&b_fd, (char *[]){b_cmd, "-first", NULL});
+
+  b_pid = popen2(&b_fd, (char *[]){b_cmd, width_s, height_s, "-first", NULL});
 
   if (b_pid < 0){
     fprintf(stderr, "Black[%s] AI failed.\n", b_cmd);
     return 1;
   }
   b_f = fdopen(b_fd, "w+");
-  
+
   printf("launching white AI: %s\n", w_cmd);
-  w_pid = popen2(&w_fd, (char *[]){w_cmd, NULL});
+  w_pid = popen2(&w_fd, (char *[]){w_cmd, width_s, height_s, NULL});
   if (w_pid < 0){
     fprintf(stderr, "White[%s] AI failed.\n", w_cmd);
     return 1;
   }
   w_f = fdopen(w_fd, "w+");
-  
+
   if (b_f == NULL || w_f == NULL){
     puts("Open pipe failed.");
     return 1;
@@ -156,7 +162,7 @@ int main (int argc, char *argv[]){
       winner = WHITE;
       break;
     }
-    
+
     if (new_move(map, x, y, &winner) != 0){
       printf("Invalid move of black: %c%d\n", 'A' + x, y);
       winner = WHITE;
@@ -183,7 +189,7 @@ int main (int argc, char *argv[]){
     }
     printf("White: %c%d\n", 'A' + x, y);    
     fprintf(b_f, "%c%d\n", 'A' + x, y);
-    
+
     print_board(map);
     // TODO: winner?
   }
@@ -196,13 +202,13 @@ int main (int argc, char *argv[]){
   kill(b_pid, 9);
 
   switch (winner) {
-  case BLACK:
-    puts("Black win."); break;
-  case WHITE:
-    puts("White win."); break;
-  case NOBODY:
-    puts("Draw."); break;
-  case NOTEXIST:  // This should not happen
-    puts("Winner not exist"); break;
+    case BLACK:
+      puts("Black win."); break;
+    case WHITE:
+      puts("White win."); break;
+    case NOBODY:
+      puts("Draw."); break;
+    case NOTEXIST:  // This should not happen
+      puts("Winner not exist"); break;
   }
 }
